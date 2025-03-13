6d8dae83b81115a5322f90c2cd546ae4b8dad76a
Davide Ricca <davidericca109@gmail.com>
2025-03-13 19:05:15 +0100

    [EHN-002]: HotFix; update environment.ts NODE_ENV

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,4 +6,6 @@ export const env = {
 
     DATABASE_URL: process.env.DATABASE_URL || 'postgres://user:password@localhost:5432/table',
     REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
+
+    NODE_ENV: process.env.NODE_ENV || 'development',
 };
