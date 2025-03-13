d99ad4c302dad74b03021512d79b2ba9546d31ac
Davide Ricca <davidericca109@gmail.com>
2025-03-13 18:55:07 +0100

    [EHN-001] creation of error handling

--- src/Interfaces/logger/LoggerService.ts
+++ src/Interfaces/logger/LoggerService.ts
@@ -1,7 +1,7 @@
 import { LogLevel } from "../../enums/LogLevelEnum";
 
 export interface ILoggerService {
-    log(level: LogLevel, message: string, args?: any): void;
+    log(level: LogLevel, message: string, args?: any[]): void;
     error(message: string): void;
     warn(message: string): void;
     info(message: string): void;
